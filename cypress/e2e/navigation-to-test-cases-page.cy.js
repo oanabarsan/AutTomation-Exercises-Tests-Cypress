3ce5ad889d2a6833f76afdecb6c3cf90e77ada1e
oana maria barsan <oanabarsan@yahoo.com>
2023-06-21 20:58:53 +0300

    add new assertion

--- cypress/e2e/navigation-to-test-cases-page.cy.js
+++ cypress/e2e/navigation-to-test-cases-page.cy.js
@@ -4,6 +4,17 @@ import HeaderPage from "../pages/HeaderPage";
 describe("Navigate to test case page test suite", () => {
   it("Navigate to test case page", () => {
     HeaderPage.getTestCaseLink().click();
+    cy.get('div.col-sm-9 h2.title.text-center')
+  .scrollIntoView()
+  .within(() => {
+    cy.window().then((win) => {
+      cy.contains('Test Cases').then(($el) => {
+        const before = win.getComputedStyle($el[0], '::before')
+        const beforeContent = before.getPropertyValue('content')
+        expect(beforeContent).to.equal('none');
+      })
+    })
+  })
     cy.get("div.panel-group h5")
       .contains(
         "Below is the list of test Cases for you to practice the Automation. Click on the scenario for detailed Test Steps:"
