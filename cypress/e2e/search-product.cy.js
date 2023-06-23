42b3b96a2962c5eec83e2247d08ac03ef3187dd4
oana maria barsan <oanabarsan@yahoo.com>
2023-06-23 19:59:16 +0300

    create search product test

---
+++ cypress/e2e/search-product.cy.js
@@ -0,0 +1,37 @@
+/// <reference types = "cypress" />
+
+import HeaderPage from "../pages/HeaderPage";
+import AllProductsPage from "../pages/AllProductsPage";
+
+describe("Search product test suite", () => {
+  it("Search product by an existing product", () => {
+    HeaderPage.getProductsLink().click();
+    cy.get("div.features_items h2.title.text-center")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("All Products").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal('" "');
+          });
+        });
+      });
+    AllProductsPage.getSearchField().type("Winter Top");
+    AllProductsPage.getSearchBtn().click();
+    cy.get("div.features_items h2.title.text-center")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("Searched Products").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal('" "');
+          });
+        });
+      });
+    cy.get("div.productinfo.text-center p	")
+      .contains("Winter Top")
+      .should("exist");
+  });
+});
--- cypress/pages/AllProductsPage.js
+++ cypress/pages/AllProductsPage.js
@@ -2,6 +2,14 @@ class AllProductsPage{
   getFirstProduct(){
     return cy.get('a[href="/product_details/1"]').contains('View Product');
   }
+
+  getSearchField(){
+    return cy.get('#search_product');
+  }
+
+  getSearchBtn(){
+    return cy.get('#submit_search');
+  }
 }
 
 export default new AllProductsPage();
