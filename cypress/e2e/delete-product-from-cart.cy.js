c5406c09102ef064169640c31d2c0ac8fec4915f
oana maria barsan <oanabarsan@yahoo.com>
2023-06-25 21:16:29 +0300

    create delete product from cart

---
+++ cypress/e2e/delete-product-from-cart.cy.js
@@ -0,0 +1,30 @@
+/// <reference types = "cypress" />
+
+import HeaderPage from "../pages/HeaderPage";
+import AllProductsPage from "../pages/AllProductsPage";
+import ThirdProductPage from "../pages/ThirdProductPage";
+
+describe("Delete product from cart page test suite", () => {
+
+  it("Delete product from cart page test", () => {
+    HeaderPage.getProductsLink().click();
+    AllProductsPage.getThirdProduct().click();
+    cy.get('div.product-information h2').contains('Sleeveless Dress').should('be.visible');
+    ThirdProductPage.getAddToCartBtn().click();
+    ThirdProductPage.getViewCartBtn().click();
+    ThirdProductPage.getCloseBtn().click();
+    cy.get('span[id="empty_cart"] p.text-center').contains(" Click ").children('b').should('have.text', "Cart is empty!");
+    ThirdProductPage.getHereLink().click({ multiple: true });
+    cy.get("div.features_items h2.title.text-center")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("All Products").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal('" "');
+          });
+        });
+      });
+  });
+});
--- cypress/pages/ThirdProductPage.js
+++ cypress/pages/ThirdProductPage.js
@@ -4,11 +4,19 @@ class ThirdProductPage{
   }
 
   getAddToCartBtn(){
-    return cy.get('button.btn.btn-default.cart').contains('Add to cart');
+    return cy.get('button.btn.btn-default.cart').contains(' Add to cart ');
   }
 
   getViewCartBtn(){
     return cy.get('a[href="/view_cart"]').contains('View Cart');
+  }
+
+  getCloseBtn(){
+    return cy.get('a.cart_quantity_delete');
+  }
+
+  getHereLink(){
+    return cy.get('a[href="/products"]').children('u').should('have.text', "here");
   }
 
 }
