fd7a652f328ec1077a891f324da12da053a1bed1
oana maria barsan <oanabarsan@yahoo.com>
2023-07-17 21:09:27 +0300

    create submit contact us form with no subject test

--- cypress/e2e/contact-us-form.cy.js
+++ cypress/e2e/contact-us-form.cy.js
@@ -34,6 +34,29 @@ describe("Contact us test suite", () => {
       .should("be.visible");
   });
 
+  it.only("Submit form with no subject inserted in subject field test", () => {
+    HeaderPage.getContactUsLink().click();
+    cy.get("div.col-sm-12 h2")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("Contact ").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal('" "');
+          });
+        });
+      });
+    ContactUsPage.getNameField().type(fullName, { delay: 0 });
+    ContactUsPage.getEmailField().type(randomEmail, { delay: 0 });
+    ContactUsPage.getParagraphField().type(randomParagraph, { delay: 0 });
+    ContactUsPage.getChooseFileBtn().selectFile("cypress/fixtures/" + fileName);
+    ContactUsPage.getSubmitBtn().click();
+    cy.get("div.status.alert.alert-success")
+      .contains("Success! Your details have been submitted successfully.")
+      .should("be.visible");
+  });
+
   it("Submit form with no name inserted in name field test", () => {
     HeaderPage.getContactUsLink().click();
     cy.get("div.col-sm-12 h2")
