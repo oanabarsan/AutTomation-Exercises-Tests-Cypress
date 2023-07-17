5de5092065e2ffd1db956c79a486786ac7a6a498
oana maria barsan <oanabarsan@yahoo.com>
2023-07-17 21:02:39 +0300

    create negative test for contact us form

--- cypress/e2e/contact-us-form.cy.js
+++ cypress/e2e/contact-us-form.cy.js
@@ -3,55 +3,80 @@ import { faker } from "@faker-js/faker";
 import HeaderPage from "../pages/HeaderPage";
 import ContactUsPage from "../pages/ContactUsPage";
 
-const fullName = faker.person.fullName()
+const fullName = faker.person.fullName();
 const randomEmail = faker.internet.email();
 const randomSubject = faker.lorem.sentence();
 const randomParagraph = faker.lorem.paragraph();
 const fileName = "463798.jpg";
 
 describe("Contact us test suite", () => {
-
   it("Contact us test", () => {
     HeaderPage.getContactUsLink().click();
-    cy.get('div.col-sm-12 h2')
-  .scrollIntoView()
-  .within(() => {
-    cy.window().then((win) => {
-      cy.contains('Contact ').then(($el) => {
-        const before = win.getComputedStyle($el[0], '::before')
-        const beforeContent = before.getPropertyValue('content')
-        expect(beforeContent).to.equal('" "');
-      })
-    })
-  })
-    ContactUsPage.getNameField().type(fullName, { delay: 0});
-    ContactUsPage.getEmailField().type(randomEmail, { delay: 0});
-    ContactUsPage.getSubjectField().type(randomSubject, { delay: 0});
-    ContactUsPage.getParagraphField().type(randomParagraph, { delay: 0});
+    cy.get("div.col-sm-12 h2")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("Contact ").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal('" "');
+          });
+        });
+      });
+    ContactUsPage.getNameField().type(fullName, { delay: 0 });
+    ContactUsPage.getEmailField().type(randomEmail, { delay: 0 });
+    ContactUsPage.getSubjectField().type(randomSubject, { delay: 0 });
+    ContactUsPage.getParagraphField().type(randomParagraph, { delay: 0 });
     ContactUsPage.getChooseFileBtn().selectFile("cypress/fixtures/" + fileName);
     ContactUsPage.getSubmitBtn().click();
-    cy.get('div.status.alert.alert-success').contains('Success! Your details have been submitted successfully.').should('be.visible');
+    cy.get("div.status.alert.alert-success")
+      .contains("Success! Your details have been submitted successfully.")
+      .should("be.visible");
   });
 
   it("Submit form with no name inserted in name field test", () => {
     HeaderPage.getContactUsLink().click();
-    cy.get('div.col-sm-12 h2')
-  .scrollIntoView()
-  .within(() => {
-    cy.window().then((win) => {
-      cy.contains('Contact ').then(($el) => {
-        const before = win.getComputedStyle($el[0], '::before')
-        const beforeContent = before.getPropertyValue('content')
-        expect(beforeContent).to.equal('" "');
-      })
-    })
-  })
-    ContactUsPage.getEmailField().type(randomEmail, { delay: 0});
-    ContactUsPage.getSubjectField().type(randomSubject, { delay: 0});
-    ContactUsPage.getParagraphField().type(randomParagraph, { delay: 0});
+    cy.get("div.col-sm-12 h2")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("Contact ").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal('" "');
+          });
+        });
+      });
+    ContactUsPage.getEmailField().type(randomEmail, { delay: 0 });
+    ContactUsPage.getSubjectField().type(randomSubject, { delay: 0 });
+    ContactUsPage.getParagraphField().type(randomParagraph, { delay: 0 });
     ContactUsPage.getChooseFileBtn().selectFile("cypress/fixtures/" + fileName);
     ContactUsPage.getSubmitBtn().click();
-    cy.get('div.status.alert.alert-success').contains('Success! Your details have been submitted successfully.').should('be.visible');
+    cy.get("div.status.alert.alert-success")
+      .contains("Success! Your details have been submitted successfully.")
+      .should("be.visible");
   });
- 
+
+  it("Try to submit form with no email inserted in email field test", () => {
+    HeaderPage.getContactUsLink().click();
+    cy.get("div.col-sm-12 h2")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("Contact ").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal('" "');
+          });
+        });
+      });
+    ContactUsPage.getNameField().type(fullName, { delay: 0 });
+    ContactUsPage.getSubjectField().type(randomSubject, { delay: 0 });
+    ContactUsPage.getParagraphField().type(randomParagraph, { delay: 0 });
+    ContactUsPage.getChooseFileBtn().selectFile("cypress/fixtures/" + fileName);
+    ContactUsPage.getSubmitBtn().click();
+    cy.get("div[id='form-section'] div.form-group.col-md-6 input[data-qa='email']:invalid")
+      .invoke("prop", "validationMessage")
+      .should("exist");
+  });
 });
