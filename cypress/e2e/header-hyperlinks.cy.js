658d5451dfe3da743c4fa343ec4f434a14f97f03
oana maria barsan <oanabarsan@yahoo.com>
2023-07-20 22:14:16 +0300

    create Test case hyperlink navigation test

--- cypress/e2e/header-hyperlinks.cy.js
+++ cypress/e2e/header-hyperlinks.cy.js
@@ -2,7 +2,7 @@
 
 import HeaderPage from "../pages/HeaderPage";
 
-describe("Header hyperlinks test suite", () => {
+describe("Header hyperlinks navigation test suite", () => {
   it("Home page hyperlink test", () => {
     HeaderPage.getHomeLink.click();
     cy.get("div.col-sm-6 h2")
@@ -33,4 +33,24 @@ describe("Header hyperlinks test suite", () => {
     HeaderPage.getSignupLink().click();
     cy.get('h2').contains('New User Signup!').should('be.visible');
   });
+
+  it("Test Cases page hyperlink test", () => {
+    HeaderPage.getTestCaseLink().click();
+    cy.get('div.col-sm-9 h2.title.text-center')
+    .scrollIntoView()
+    .within(() => {
+      cy.window().then((win) => {
+        cy.contains('Test Cases').then(($el) => {
+          const before = win.getComputedStyle($el[0], '::before')
+          const beforeContent = before.getPropertyValue('content')
+          expect(beforeContent).to.equal('none');
+        })
+      })
+    })
+      cy.get("div.panel-group h5")
+        .contains(
+          "Below is the list of test Cases for you to practice the Automation. Click on the scenario for detailed Test Steps:"
+        )
+        .should("exist");
+    });
 });
