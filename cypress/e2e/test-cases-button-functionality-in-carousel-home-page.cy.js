28498ee8a1aacbbbeeb97a5253838c9177f9358a
oana maria barsan <oanabarsan@yahoo.com>
2023-07-23 14:52:25 +0300

    create test cases button functionality from third carousel image in home page

--- cypress/e2e/test-cases-button-functionality-in-carousel-home-page.cy.js
+++ cypress/e2e/test-cases-button-functionality-in-carousel-home-page.cy.js
@@ -22,7 +22,7 @@ describe("Test Cases button functionality in carousel home page test suite", () 
       )
       .should("exist");
   });
-  
+
   it("Test Cases button functionality located in second carousel image test", () => {
     CarouselHomePage.getSecondTestCaseBtn().click();
     cy.get("div.col-sm-9 h2.title.text-center")
@@ -42,5 +42,23 @@ describe("Test Cases button functionality in carousel home page test suite", () 
       )
       .should("exist");
   });
-
+  it("Test Cases button functionality located in third carousel image test", () => {
+    CarouselHomePage.getThirdTestCaseBtn().click();
+    cy.get("div.col-sm-9 h2.title.text-center")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("Test Cases").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal("none");
+          });
+        });
+      });
+    cy.get("div.panel-group h5")
+      .contains(
+        "Below is the list of test Cases for you to practice the Automation. Click on the scenario for detailed Test Steps:"
+      )
+      .should("exist");
+  });
 });
