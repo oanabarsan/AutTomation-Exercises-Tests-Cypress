693c9a62b879cafcb826d55c85d8ab3811f5041b
oana maria barsan <oanabarsan@yahoo.com>
2023-07-23 14:51:43 +0300

    create test case button functionality from second carousel image in home page

--- cypress/e2e/test-cases-button-functionality-in-carousel-home-page.cy.js
+++ cypress/e2e/test-cases-button-functionality-in-carousel-home-page.cy.js
@@ -22,5 +22,25 @@ describe("Test Cases button functionality in carousel home page test suite", () 
       )
       .should("exist");
   });
+  
+  it("Test Cases button functionality located in second carousel image test", () => {
+    CarouselHomePage.getSecondTestCaseBtn().click();
+    cy.get("div.col-sm-9 h2.title.text-center")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("Test Cases").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal("none");
+          });
+        });
+      });
+    cy.get("div.panel-group h5")
+      .contains(
+        "Below is the list of test Cases for you to practice the Automation. Click on the scenario for detailed Test Steps:"
+      )
+      .should("exist");
+  });
 
 });
