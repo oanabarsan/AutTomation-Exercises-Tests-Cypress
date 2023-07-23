9a7818016f17ae758324e1ff1667861ae272f320
oana maria barsan <oanabarsan@yahoo.com>
2023-07-23 17:46:58 +0300

    create alternative movements between next and prev arrows test

--- cypress/e2e/top-carousel-arrows-functionality-in-home-page.cy.js
+++ cypress/e2e/top-carousel-arrows-functionality-in-home-page.cy.js
@@ -20,4 +20,16 @@ describe("Top carousel arrows functionality test suite", () => {
     TopCarouselHomePage.getNextArrow().click();
     cy.get('img[src="/static/images/home/girl2.jpg"]').should("exist");
   });
+
+  it("Next and Prev arrow carousel test", () => {
+    TopCarouselHomePage.getNextArrow().click();
+    cy.get('img[src="/static/images/home/girl1.jpg"]').should("exist");
+    TopCarouselHomePage.getPrevArrow().click();
+    cy.get('img[src="/static/images/home/girl2.jpg"]').should("exist");
+    TopCarouselHomePage.getNextArrow().click();
+    cy.get('img[src="/static/images/home/girl1.jpg"]').should("exist");
+    TopCarouselHomePage.getPrevArrow().click();
+    TopCarouselHomePage.getPrevArrow().click();
+    cy.get('img[src="/static/images/home/girl3.jpg"]').should("exist");
+  });
 });
