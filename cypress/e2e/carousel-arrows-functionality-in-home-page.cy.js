78b3a02a2d532333c24a72bb27223004806cedb9
oana maria barsan <oanabarsan@yahoo.com>
2023-07-23 16:37:23 +0300

    change cod format

--- cypress/e2e/carousel-arrows-functionality-in-home-page.cy.js
+++ cypress/e2e/carousel-arrows-functionality-in-home-page.cy.js
@@ -5,21 +5,19 @@ import CarouselHomePage from "../pages/CarouselHomePage";
 describe("Carousel arrows functionality test suite", () => {
   it("Prev arrow carousel test", () => {
     CarouselHomePage.getPrevArrow().click();
-    cy.get('img[src="/static/images/home/girl1.jpg"]').should('exist');
+    cy.get('img[src="/static/images/home/girl1.jpg"]').should("exist");
     CarouselHomePage.getPrevArrow().click();
-    cy.get('img[src="/static/images/home/girl3.jpg"]').should('exist');
+    cy.get('img[src="/static/images/home/girl3.jpg"]').should("exist");
     CarouselHomePage.getPrevArrow().click();
-    cy.get('img[src="/static/images/home/girl2.jpg"]').should('exist');
+    cy.get('img[src="/static/images/home/girl2.jpg"]').should("exist");
   });
 
   it("Next arrow carousel test", () => {
     CarouselHomePage.getNextArrow().click();
-    cy.get('img[src="/static/images/home/girl3.jpg"]').should('exist');
+    cy.get('img[src="/static/images/home/girl3.jpg"]').should("exist");
     CarouselHomePage.getPrevArrow().click();
-    cy.get('img[src="/static/images/home/girl1.jpg"]').should('exist');
+    cy.get('img[src="/static/images/home/girl1.jpg"]').should("exist");
     CarouselHomePage.getPrevArrow().click();
-    cy.get('img[src="/static/images/home/girl2.jpg"]').should('exist');
+    cy.get('img[src="/static/images/home/girl2.jpg"]').should("exist");
   });
-
-
 });
