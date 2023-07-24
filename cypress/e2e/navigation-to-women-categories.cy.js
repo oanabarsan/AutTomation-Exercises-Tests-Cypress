65aab30d041ae54e5a0a6ed6de3c226af69a301b
oana maria barsan <oanabarsan@yahoo.com>
2023-07-24 20:31:43 +0300

    modify file name

---
+++ cypress/e2e/navigation-to-women-categories.cy.js
@@ -0,0 +1,51 @@
+/// <reference types = "cypress" />
+
+import HomeCategoryLinksPage from "../pages/HomeCategoryLinksPage";
+
+describe("Navigation to Women categories in home page test suite", () => {
+  beforeEach(() => {
+    HomeCategoryLinksPage.getWomenCategoryLink().click();
+  });
+  it("Navigate to Dress category test", () => {
+    HomeCategoryLinksPage.getDressCategory().click();
+    cy.get("div.features_items h2.title.text-center")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("Women - Dress Products").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal('" "');
+          });
+        });
+      });
+  });
+  it("Navigate to Tops category test", () => {
+    HomeCategoryLinksPage.getTopsCategory().click();
+    cy.get("div.features_items h2.title.text-center")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("Women - Tops Products").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal('" "');
+          });
+        });
+      });
+  });
+  it("Navigate to Saree category test", () => {
+    HomeCategoryLinksPage.getSareeCategory().click();
+    cy.get("div.features_items h2.title.text-center")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("Women - Saree Products").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal('" "');
+          });
+        });
+      });
+  });
+});
