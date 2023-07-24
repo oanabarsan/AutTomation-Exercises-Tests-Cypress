ae888fbaaeb7af1219272aaed8ee8754db14695b
oana maria barsan <oanabarsan@yahoo.com>
2023-07-24 20:29:13 +0300

    create navigation to tops category

--- cypress/e2e/navigation-to-women-category.cy.js
+++ cypress/e2e/navigation-to-women-category.cy.js
@@ -1,13 +1,12 @@
 /// <reference types = "cypress" />
 
-import HeaderPage from "../pages/HeaderPage";
 import HomeCategoryLinksPage from "../pages/HomeCategoryLinksPage";
 
 describe("Navigation to Women categories in home page test suite", () => {
   beforeEach(() => {
     HomeCategoryLinksPage.getWomenCategoryLink().click();
   });
-  it("Navigate to dress category test", () => {
+  it("Navigate to Dress category test", () => {
     HomeCategoryLinksPage.getDressCategory().click();
     cy.get("div.features_items h2.title.text-center")
       .scrollIntoView()
@@ -21,7 +20,6 @@ describe("Navigation to Women categories in home page test suite", () => {
         });
       });
   });
-
   it("Navigate to Tops category test", () => {
     HomeCategoryLinksPage.getTopsCategory().click();
     cy.get("div.features_items h2.title.text-center")
@@ -37,18 +35,4 @@ describe("Navigation to Women categories in home page test suite", () => {
       });
   });
 
-  it("Navigate to Saree category test", () => {
-    HomeCategoryLinksPage.getSareeCategory().click();
-    cy.get("div.features_items h2.title.text-center")
-      .scrollIntoView()
-      .within(() => {
-        cy.window().then((win) => {
-          cy.contains("Women - Saree Products").then(($el) => {
-            const before = win.getComputedStyle($el[0], "::before");
-            const beforeContent = before.getPropertyValue("content");
-            expect(beforeContent).to.equal('" "');
-          });
-        });
-      });
-  });
 });
