c7138d38b1d48fd43a239fa7737742f335a26a4d
oana maria barsan <oanabarsan@yahoo.com>
2023-07-24 20:30:01 +0300

    create navigation to saree category

--- cypress/e2e/navigation-to-women-category.cy.js
+++ cypress/e2e/navigation-to-women-category.cy.js
@@ -34,5 +34,18 @@ describe("Navigation to Women categories in home page test suite", () => {
         });
       });
   });
-
+  it("Navigate to Saree category test", () => {
+    HomeCategoryLinksPage.getSareeCategory().click();
+    cy.get("div.features_items h2.title.text-center")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("Women - Saree Products").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal('" "');
+          });
+        });
+      });
+  });
 });
