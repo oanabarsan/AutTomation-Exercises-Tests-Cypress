e045f98fda526d4565e8f728f1691d1a755a9758
oana maria barsan <oanabarsan@yahoo.com>
2023-07-25 17:47:17 +0300

    create navigate to Babyhug brand category test

--- cypress/pages/BrandsCategoryPage.js
+++ cypress/pages/BrandsCategoryPage.js
@@ -14,6 +14,10 @@ class BrandsCategoryPage{
   getMastAndHarbourBrandLink(){
     return cy.get("ul.nav.nav-pills.nav-stacked li a[href='/brand_products/Mast & Harbour']");
   }
+
+  getBabyhugBrandLink(){
+    return cy.get("ul.nav.nav-pills.nav-stacked li a[href='/brand_products/Babyhug']");
+  }
 }
 
 export default new BrandsCategoryPage();
