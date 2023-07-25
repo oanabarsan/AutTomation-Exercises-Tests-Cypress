497c23112b95420f79a98b1c85685d5878696332
oana maria barsan <oanabarsan@yahoo.com>
2023-07-25 17:28:30 +0300

    create navigation to H&M brand category test

--- cypress/e2e/navigation-to-brands-category.cy.js
+++ cypress/e2e/navigation-to-brands-category.cy.js
@@ -17,5 +17,20 @@ describe("Navigation to Brands categories in home page test suite", () => {
         });
       });
   });
+
+  it.only("Navigate to H&M category test", () => {
+    BrandsCategoryPage.getHnMBrandLink().click();
+    cy.get("div.features_items h2.title.text-center")
+      .scrollIntoView()
+      .within(() => {
+        cy.window().then((win) => {
+          cy.contains("Brand - H&M ").then(($el) => {
+            const before = win.getComputedStyle($el[0], "::before");
+            const beforeContent = before.getPropertyValue("content");
+            expect(beforeContent).to.equal('" "');
+          });
+        });
+      });
+  });
   
 });
--- cypress/pages/BrandsCategoryPage.js
+++ cypress/pages/BrandsCategoryPage.js
@@ -2,6 +2,10 @@ class BrandsCategoryPage{
   getPoloBrandLink(){
     return cy.get("ul.nav.nav-pills.nav-stacked li a[href='/brand_products/Polo']");
   }
+
+  getHnMBrandLink(){
+    return cy.get("ul.nav.nav-pills.nav-stacked li a[href='/brand_products/H&M']");
+  }
 }
 
 export default new BrandsCategoryPage();
